e17c1ec43e7096313e90a0c65895f989013f008b
yunsehwan <sh10302@naver.com>
2022-12-07 12:44:59 +0900

    [fix] PostCard scaleType 이미지 속성 변경 (crop으로 변경)

--- app/src/main/java/com/strayalpaca/android/abox/ui/components/PostCard.kt
+++ app/src/main/java/com/strayalpaca/android/abox/ui/components/PostCard.kt
@@ -13,20 +13,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.strayalpaca.android.abox.ui.shapes.ImageInCardShape
 import com.strayalpaca.android.abox.ui.theme.ABOXTheme
 
 @Composable
-fun PostCard(imageSizeDp: Int = 225) {
-
+fun PostCard(modifier: Modifier = Modifier, imageSizeDp: Int = 225) {
     val heightDiff = 36
 
     Surface(
         shape = RoundedCornerShape(16.dp),
         border = BorderStroke(width = 1.dp, color = MaterialTheme.colors.onSurface),
+        modifier = modifier
     ) {
         Row(
             modifier = Modifier
@@ -40,32 +42,9 @@ fun PostCard(imageSizeDp: Int = 225) {
                     .height((imageSizeDp * 2).dp)
                     .border(1.dp, MaterialTheme.colors.onSurface)
             ) {
-//                AsyncImage(
-//                    model = "https://picsum.photos/seed/picsum/500/500",
-//                    contentDescription = "Image A",
-//                    modifier = Modifier
-//                        .offset(x = 0.dp, y = 0.dp)
-//                        .width(imageSizeDp.dp)
-//                        .height((imageSizeDp + heightDiff / 2).dp)
-//                        .graphicsLayer {
-//                            shape = ImageInCardShape(heightDiff.dp.roundToPx(), isTop = true)
-//                            clip = true
-//                        }
-//                        .background(MaterialTheme.colors.primary)
-//                AsyncImage(
-//                    model = "https://picsum.photos/seed/picsum/500/500",
-//                    contentDescription = "Image B",
-//                    modifier = Modifier
-//                        .offset(x = 0.dp, y = 0.dp)
-//                        .width(imageSizeDp.dp)
-//                        .height((imageSizeDp + heightDiff / 2).dp)
-//                        .graphicsLayer {
-//                            shape = ImageInCardShape(heightDiff.dp.roundToPx(), isTop = false)
-//                            clip = true
-//                        }
-//                        .background(MaterialTheme.colors.primary)
-
-                Box(
+                AsyncImage(
+                    model = "https://picsum.photos/seed/picsum/500/500",
+                    contentDescription = "Image A",
                     modifier = Modifier
                         .offset(x = 0.dp, y = 0.dp)
                         .width(imageSizeDp.dp)
@@ -73,12 +52,12 @@ fun PostCard(imageSizeDp: Int = 225) {
                         .graphicsLayer {
                             shape = ImageInCardShape(heightDiff.dp.roundToPx(), isTop = true)
                             clip = true
-                        }
-                        .background(MaterialTheme.colors.secondary)
-                ) {
-
-                }
-                Box(
+                        },
+                    contentScale = ContentScale.Crop
+                )
+                AsyncImage(
+                    model = "https://picsum.photos/seed/picsum/500/500",
+                    contentDescription = "Image B",
                     modifier = Modifier
                         .offset(x = 0.dp, y = (imageSizeDp - heightDiff / 2).dp)
                         .width(imageSizeDp.dp)
@@ -86,11 +65,9 @@ fun PostCard(imageSizeDp: Int = 225) {
                         .graphicsLayer {
                             shape = ImageInCardShape(heightDiff.dp.roundToPx(), isTop = false)
                             clip = true
-                        }
-                        .background(MaterialTheme.colors.secondaryVariant)
-                ) {
-
-                }
+                        },
+                    contentScale = ContentScale.Crop
+                )
             }
             Text(text = "B", fontSize = 38.sp, modifier = Modifier.align(Alignment.Bottom))
         }
@@ -101,7 +78,54 @@ fun PostCard(imageSizeDp: Int = 225) {
 @Preview(name = "dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PostCardPreview() {
+    val imageSizeDp = 225
+    val heightDiff = 36
     ABOXTheme {
-        PostCard()
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            border = BorderStroke(width = 1.dp, color = MaterialTheme.colors.onSurface),
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(MaterialTheme.colors.surface)
+                    .padding(12.dp)
+            ) {
+                Text(text = "A", fontSize = 38.sp)
+                Box(
+                    Modifier
+                        .padding(vertical = 19.dp)
+                        .height((imageSizeDp * 2).dp)
+                        .border(1.dp, MaterialTheme.colors.onSurface)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .offset(x = 0.dp, y = 0.dp)
+                            .width(imageSizeDp.dp)
+                            .height((imageSizeDp + heightDiff / 2).dp)
+                            .graphicsLayer {
+                                shape = ImageInCardShape(heightDiff.dp.roundToPx(), isTop = true)
+                                clip = true
+                            }
+                            .background(MaterialTheme.colors.secondary)
+                    ) {
+
+                    }
+                    Box(
+                        modifier = Modifier
+                            .offset(x = 0.dp, y = (imageSizeDp - heightDiff / 2).dp)
+                            .width(imageSizeDp.dp)
+                            .height((imageSizeDp + heightDiff / 2).dp)
+                            .graphicsLayer {
+                                shape = ImageInCardShape(heightDiff.dp.roundToPx(), isTop = false)
+                                clip = true
+                            }
+                            .background(MaterialTheme.colors.secondaryVariant)
+                    ) {
+
+                    }
+                }
+                Text(text = "B", fontSize = 38.sp, modifier = Modifier.align(Alignment.Bottom))
+            }
+        }
     }
 }
