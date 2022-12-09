e9bf82fb9c459d0b24bbbe2b7aebf46a5c36ad7e
yunsehwan <sh10302@naver.com>
2022-12-09 13:29:04 +0900

    [fix] dotList에서 라운드 종료시 등장/퇴장 애니메이션 수정

--- app/src/main/java/com/strayalpaca/android/abox/ui/components/ListDot.kt
+++ app/src/main/java/com/strayalpaca/android/abox/ui/components/ListDot.kt
@@ -8,41 +8,43 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @Composable
-fun ListDot(modifier: Modifier = Modifier, amountOfItem: Int, currentPosition: Int) {
-    val alpha = remember { Animatable(0f) }
-    val offsetY = remember { Animatable(0f) }
-    val positionList = remember { mutableStateListOf<Int>() }
+fun ListDot(modifier: Modifier = Modifier, round: Int, currentPosition: Int) {
+    val alpha = remember { Animatable(initialValue = 0f) }
+    val offsetY = remember { Animatable(initialValue = 0f) }
+    val amountOfItem = remember { mutableStateOf(value = round / 2) }
 
-    LaunchedEffect(amountOfItem) {
-        positionList.clear()
-        positionList.addAll((0 until amountOfItem).toList())
+    LaunchedEffect(round) {
         launch {
             alpha.animateTo(targetValue = 0f, animationSpec = tween(500))
+            amountOfItem.value = round / 2
             alpha.animateTo(targetValue = 1f, animationSpec = tween(500))
         }
         launch {
-            offsetY.animateTo(targetValue = -20f, animationSpec = tween(500))
-            offsetY.snapTo(20f)
+            offsetY.animateTo(targetValue = -16f, animationSpec = tween(500))
+            offsetY.snapTo(16f)
             offsetY.animateTo(targetValue = 0f, animationSpec = tween(500))
         }
     }
 
     Column(
-        modifier = modifier,
+        modifier = modifier.alpha(alpha.value).offset { IntOffset(x = 0, y = offsetY.value.roundToInt()) }.padding(vertical = 16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        for (i in 0 until amountOfItem step (8)) {
+        for (i in 0 until amountOfItem.value step (8)) {
             Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
                 for (j in 0 until 8) {
                     val index = i + j
-                    if (index < amountOfItem) {
-                        key("${amountOfItem}_${index}") {
+                    if (index < amountOfItem.value) {
+                        key("${amountOfItem.value}_${index}") {
                             NormalDot(
                                 isCurrentPosition = index == currentPosition,
                                 isPrevPosition = index == (currentPosition - 1)
